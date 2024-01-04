9515bc3464894ff2269edd7061705691869659f2
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2024-01-04 16:40:55 +0530

    Update kora.py

--- kora.py
+++ kora.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
 system ("rm -rf AnjaliGroupManagerBOT && git clone https://github.com/mahtoanjali/AnjaliGroupManagerBOT && cd AnjaliGroupManagerBOT && python3 -m Am")
 # try af
-print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO QUEENx_GOD)
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @QUEENx_GOD")
